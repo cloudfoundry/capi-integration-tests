79c6a5c328371ed2eda46c17c6520becccb52898
Simon Leung <leungs@us.ibm.com>
2016-08-29 14:50:25 -0700

    Use environment variables to set app names
    [#129155339]
    
    Signed-off-by: Utako Ueda <uueda@pivotal.io>

--- migration/migration_suite_test.go
+++ migration/migration_suite_test.go
@@ -10,12 +10,8 @@ import (
 )
 
 const (
-	APP_NAME                      = "meow"
-	APP_WITH_ENV_VARS             = "meow"
-	APP_WITH_SERVICE_BINDING_NAME = "service-app"
-	APP_WITH_MULTIPLE_ROUTES_NAME = "woof"
-	DEFAULT_TIMEOUT               = 30 * time.Second
-	CF_PUSH_TIMEOUT               = 2 * time.Minute
+	DEFAULT_TIMEOUT = 30 * time.Second
+	CF_PUSH_TIMEOUT = 2 * time.Minute
 )
 
 func TestMigration(t *testing.T) {
